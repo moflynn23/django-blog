d5ede687d32af711ee788437553d67263274ff8c
Mark O'Flynn <mark.oflynn1@gmail.com>
2024-02-28 17:02:20 +0000

    Add environmental variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fl_2m*pvgcoyzllx((2gxsdyin3-a&fb_(ue+uo0c39x!3c&-a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
