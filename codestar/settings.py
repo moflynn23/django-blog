3bde68280807e41963589aa169528cc1ebe77c5b
Mark O'Flynn <mark.oflynn1@gmail.com>
2024-02-28 17:41:58 +0000

    Adds a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -88,6 +88,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://8000"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
